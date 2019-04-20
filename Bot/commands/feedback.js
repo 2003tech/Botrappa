65e81b1645700025d7efaa48fed85521b11b3334
PF94 <45898787+BlocklandID47101@users.noreply.github.com>
2019-04-20 13:49:24 -0400

    New Feedback

--- Bot/commands/feedback.js
+++ Bot/commands/feedback.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 
 exports.run = function(bot, message, args) {
     if(!args[0]){
-       message.channel.send("Please add something after the command.")
+       message.channel.send("Please add your feedback after the command.")
     }
     message.channel.send("Feedback sent!")
     const embed = new Discord.RichEmbed()
@@ -10,5 +10,5 @@ exports.run = function(bot, message, args) {
     .addField("User", message.author.tag)
     .addField("Feedback", args.join(" "))
     .setColor(message.guild.me.displayHexColor)
-    bot.channels.get('557686423713611814').send({embed, embed})
+    bot.channels.get('567032853188313088').send({embed, embed})
 }
