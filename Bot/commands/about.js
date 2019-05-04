8d0178b21ebad8fcab02953187d00a5fc12456b4
PF94 <45898787+BlocklandID47101@users.noreply.github.com>
2019-05-04 11:29:54 -0400

    Update about.js

--- Bot/commands/about.js
+++ Bot/commands/about.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 exports.run = function(bot, message) {
     const embed = new Discord.RichEmbed()
     .setAuthor("About Page", bot.user.avatarURL)
-    .setTitle("Warning: Beta")
+    .setTitle("Warning: This bot is incomplete. It may not have a lot of commands.")
     .addField("This bot was made by the team of", "Gamerappa (Parappafan98), Memes and XPUser")
     .addField("Special thanks to", "These guys up me")
     .setColor(message.guild.me.displayHexColor)
